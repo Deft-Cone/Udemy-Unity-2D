a4cac50a942aa24bc45ab34ef899cd5bc22212aa
Lane Alexander <51537771+Deft-Cone@users.noreply.github.com>
2023-06-13 00:21:23 -0400

    Second Commit

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        // Move the car forward
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, -steerAmount);
+    }
+}
