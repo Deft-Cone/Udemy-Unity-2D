14d33b0ff41474129981317abe696bbf435d2bfb
Lane Alexander <51537771+Deft-Cone@users.noreply.github.com>
2024-08-25 18:07:00 -0400

    41. Take a Moment To Tune

---
+++ Assets/Snowboarder/Scripts/Crash Detector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            Debug.Log("Hit your head.");
+            crashEffect.Play();
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+    
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
